4cb967f20fd42aa5a6aad2191c12dca04b05b680
Abhinav Srivastava <atg271@gmail.com>
2022-12-08 14:31:04 -0500

    wip

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,10 +1,9 @@
 import os
 import time
 from flask import Flask
-import json 
+import json
 
 app = Flask(__name__)
-
 
 class metadata:
     DEFAULT_PORT = 8080
@@ -22,6 +21,9 @@ class Server:
 
     def isActive(self):
         return self.active
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def __repr__(self):
         return f"{self.name} ({self.ip})"
@@ -52,29 +54,37 @@ class LoadBalancer:
     MAX_SERVERS = 3
     CURRENT_SERVERS = 2
     CURRENT_SYSTEMS = 5
-    systems = [Server("server-0", metadata.public_ip["server-0"], metadata.DEFAULT_PORT), Server("server-1", metadata.public_ip["server-1"], metadata.DEFAULT_PORT), Server("server-2",
-                                                                                                                                                                            metadata.public_ip["server-2"], metadata.DEFAULT_PORT), Server("router", metadata.public_ip["router"], metadata.DEFAULT_PORT), Server("client", metadata.public_ip["client"], metadata.DEFAULT_PORT)]
+    systems = [Server("server-0", metadata.public_ip["server-0"], metadata.DEFAULT_PORT), Server("server-1", metadata.public_ip["server-1"], metadata.DEFAULT_PORT), Server("server-2", metadata.public_ip["server-2"], metadata.DEFAULT_PORT), Server("router", metadata.public_ip["router"], metadata.DEFAULT_PORT), Server("client", metadata.public_ip["client"], metadata.DEFAULT_PORT)]
+
+    server_dict = {"server-0": systems[0], "server-1": systems[1], "server-2": systems[2], "router": systems[3], "client": systems[4]}
 
     @app.route('/status')
     def get_systems():
-        # Return active status of Server
-        return str(LoadBalancer.systems)
+        return HelperFunctions.turn_to_jsonack(str(LoadBalancer.systems))
 
     @app.route('/ping')
     def get_system_status():
+        print("Testing...")
+        HelperFunctions.test_all_servers()
         to_return = []
         for system in LoadBalancer.systems:
             to_return.append(system.ping())
-        return str(to_return)
+        return HelperFunctions.turn_to_jsonack(to_return)
 
     @app.route('/activity')
     def get_system_activity():
-        to_return = []
+        to_return = {}
         for system in LoadBalancer.systems:
-            to_return.append(system.name + ": " + str(system.isActive()))
-        return str(to_return)
+            to_return[system.name] = system.connections
+        return HelperFunctions.turn_to_jsonack(str(to_return))
+
 
 class HelperFunctions:
+
+    def turn_to_jsonack(data):
+        # Add  json.dumps({'success':True}), 200, {'ContentType':'application/json'} and data to another field called data
+        json_response = {"success": True, "data": data}
+        return json_response
 
     def ping(ip, port):
         hostname = ip+":"+str(port)
@@ -84,10 +94,7 @@ class HelperFunctions:
     # test all servers and update loadbalancers
     def test_all_servers():
         for server in LoadBalancer.systems:
-            if HelperFunctions.ping(server.ip, metadata.DEFAULT_PORT) == 0:
-                server.active = True
-            else:
-                server.active = False
+            test_server(server)
 
     def test_server(server):
         if HelperFunctions.ping(server.ip, metadata.DEFAULT_PORT) == 0:
@@ -97,9 +104,8 @@ class HelperFunctions:
 
     @app.route('/')
     def hello_world():
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
-    HelperFunctions.test_all_servers()
-    app.run(host="0.0.0.0", port = 8080, debug = True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
