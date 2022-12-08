747644e4670e40c725b79ece2a925d2c83043ad5
Abhinav Srivastava <atg271@gmail.com>
2022-12-08 13:42:01 -0500

    wip

--- loadbalancer.py
+++ loadbalancer.py
@@ -96,4 +96,4 @@ print(HelperFunctions.ping(
 print(LoadBalancer.get_systems())
 
 if __name__ == '__main__':
-    app.run(port = 8080, debug = True)
+    app.run(host="0.0.0.0", port = 8080, debug = True)
