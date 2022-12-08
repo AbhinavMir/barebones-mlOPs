9ea8394ba93cb4f1e333e97dc48b0b000e15e6e4
Abhinav Srivastava <atg271@gmail.com>
2022-12-08 18:00:01 -0500

    wip

--- loadbalancer.py
+++ loadbalancer.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 class metadata:
     queueCoutner = 0
+    queue = {}
     DEFAULT_PORT = 8080
     public_ip = {"server-0": "128.95.190.67", "server-1": "128.95.190.68",
                  "server-2": "128.95.190.69", "router": "128.95.190.66", "client": "128.95.190.64"}
@@ -18,9 +19,6 @@ def send_image(image, server):
     response = requests.post("http://" + server.ip + ":" + str(server.port), files=files)
     return response
 
-@app.route("/track")
-def get_IP():
-     return jsonify({'ip': request.remote_addr}), 200
 
 @app.route("/upload", methods=["GET", "POST"])
 @app.route("/", methods=['POST'])
@@ -52,7 +50,7 @@ class Server:
 
 class LoadBalancer:
     MAX_SERVERS = 3
-    CURRENT_SERVERS = 2
+    CURRENT_SERVERS = 3
     CURRENT_SYSTEMS = 5
     systems = [Server("server-0", metadata.public_ip["server-0"], metadata.DEFAULT_PORT), Server("server-1", metadata.public_ip["server-1"], metadata.DEFAULT_PORT), Server("server-2", metadata.public_ip["server-2"], metadata.DEFAULT_PORT), Server("router", metadata.public_ip["router"], metadata.DEFAULT_PORT), Server("client", metadata.public_ip["client"], metadata.DEFAULT_PORT)]
 
@@ -66,18 +64,28 @@ class HelperFunctions:
 
     servers = LoadBalancer.systems
 
+
+    @app.route("/track")
+    def get_IP():
+        return request.remote_addr
+
     @app.route('/busyChecker')
     def get_busy_status():
         busy = []
         not_busy = []
+        inactive = []
         for i in range(LoadBalancer.CURRENT_SERVERS):
             # ping server/busy
-            response = requests.get("http://" + LoadBalancer.systems[i].ip + ":" + str(LoadBalancer.systems[i].port) + "/isBusy")
+            try:
+                response = requests.get("http://" + LoadBalancer.systems[i].ip + ":" + str(LoadBalancer.systems[i].port) + "/isBusy")
+            except:
+                inactive.append(LoadBalancer.systems[i])
+                continue
             if(response.json()["busy"]):
                 busy.append(LoadBalancer.systems[i])
             else:
                 not_busy.append(LoadBalancer.systems[i])
-        return "busy: " + str(busy) + "\nNot busy:  " + str(not_busy)
+        return "Busy: " + str(busy) + "\nNot busy:  " + str(not_busy) + "\nInactive: " + str(inactive)
 
     def get_queue():
         queue = {}
