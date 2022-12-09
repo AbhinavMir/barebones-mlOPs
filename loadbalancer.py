c61ccd622f345e0049ffaccc8d64e6238124ab22
Abhinav Srivastava <atg271@gmail.com>
2022-12-08 20:09:05 -0500

    wip

--- loadbalancer.py
+++ loadbalancer.py
@@ -5,8 +5,15 @@ import json
 from PIL import Image
 import requests
 import random
+import threading
 
 app = Flask(__name__)
+
+results_dict = {}
+
+@app.route("/resultsAll")
+def resultsAll():
+    return results_dict
 
 class metadata:
     queueCoutner = 0
@@ -17,21 +24,28 @@ class metadata:
     public_ip = {"server-0": "128.95.190.67", "server-1": "128.95.190.68",
                  "server-2": "128.95.190.69", "router": "128.95.190.66", "client": "128.95.190.64"}
 
-# show all images in the static folder 
+# show all images in the static folder
+
+
 @app.route('/images')
 def images():
     # return names of all images in the static folder without using render_template
     return jsonify(os.listdir('static'))
 
+
 def send_image(image, server):
     Image = open(image, 'rb')
     files = {'myImage': Image}
-    response = requests.post("http://" + server.ip + ":" + str(server.port) + '/imagenet', files=files)
+    response = requests.post("http://" + server.ip +
+                             ":" + str(server.port) + '/imagenet', files=files)
     return response.text
 
+
 def test_this_server(server):
-    response = requests.get("http://" + server.ip + ":" + str(server.port) + "/test")
+    response = requests.get("http://" + server.ip +
+                            ":" + str(server.port) + "/test")
     return response.text
+
 
 @app.route("/upload", methods=["GET", "POST"])
 @app.route("/", methods=['POST'])
@@ -42,15 +56,16 @@ def upload():
         im = Image.open(image)
         ext = os.path.splitext(image.filename)
         # create a random number
-        fn = random.randint(0, 100000)
-        im.save("static/" + ext[0] + str(fn) + ext[1])
+        im.save("static/" + ext[0] + "-(" + str(request.remote_addr) + ")" + ext[1])
         metadata.queueCoutner += 1
-    else :
+        results_dict[request.remote_addr] = "Processing"
+    else:
         return "Please use POST, not GET"
 
 @app.route("/queue")
 def get_queue():
     return metadata.queue
+
 
 class Server:
     def __init__(self, name, ip, port):
@@ -68,26 +83,25 @@ class Server:
     def __repr__(self):
         return f"{self.name} ({self.ip})"
 
+
 class LoadBalancer:
     MAX_SERVERS = 3
     CURRENT_SERVERS = 3
     CURRENT_SYSTEMS = 5
-    systems = [Server("server-0", metadata.public_ip["server-0"], metadata.DEFAULT_PORT), Server("server-1", metadata.public_ip["server-1"], metadata.DEFAULT_PORT), Server("server-2", metadata.public_ip["server-2"], metadata.DEFAULT_PORT), Server("router", metadata.public_ip["router"], metadata.DEFAULT_PORT), Server("client", metadata.public_ip["client"], metadata.DEFAULT_PORT)]
+    systems = [Server("server-0", metadata.public_ip["server-0"], metadata.DEFAULT_PORT), Server("server-1", metadata.public_ip["server-1"], metadata.DEFAULT_PORT), Server("server-2",
+                                                                                                                                                                            metadata.public_ip["server-2"], metadata.DEFAULT_PORT), Server("router", metadata.public_ip["router"], metadata.DEFAULT_PORT), Server("client", metadata.public_ip["client"], metadata.DEFAULT_PORT)]
 
-    server_dict = {"server-0": systems[0], "server-1": systems[1], "server-2": systems[2], "router": systems[3], "client": systems[4]}
+    server_dict = {"server-0": systems[0], "server-1": systems[1],
+                   "server-2": systems[2], "router": systems[3], "client": systems[4]}
 
     @app.route('/status')
     def get_systems():
         return HelperFunctions.turn_to_jsonack(str(LoadBalancer.systems))
 
+
 class HelperFunctions:
 
     servers = LoadBalancer.systems
-
-
-    def send_final_to_server():
-        # check if there are free servers
-        HelperFunctions.get_busy_status()
 
     @app.route("/track")
     def get_IP():
@@ -101,16 +115,17 @@ class HelperFunctions:
         for i in range(LoadBalancer.CURRENT_SERVERS):
             # ping server/busy
             try:
-                response = requests.get("http://" + LoadBalancer.systems[i].ip + ":" + str(LoadBalancer.systems[i].port) + "/isBusy")
+                response = requests.get(
+                    "http://" + LoadBalancer.systems[i].ip + ":" + str(LoadBalancer.systems[i].port) + "/isBusy")
             except:
                 inactive.append(LoadBalancer.systems[i])
                 continue
-            if(response.json()["busy"]):
+            if (response.json()["busy"]):
                 busy.append(LoadBalancer.systems[i])
             else:
                 not_busy.append(LoadBalancer.systems[i])
         return str({"busy": busy, "not_busy": not_busy, "inactive": inactive})
-    
+
     def get_server_by_function():
         busy = []
         not_busy = []
@@ -118,11 +133,12 @@ class HelperFunctions:
         for i in range(LoadBalancer.CURRENT_SERVERS):
             # ping server/busy
             try:
-                response = requests.get("http://" + LoadBalancer.systems[i].ip + ":" + str(LoadBalancer.systems[i].port) + "/isBusy")
+                response = requests.get(
+                    "http://" + LoadBalancer.systems[i].ip + ":" + str(LoadBalancer.systems[i].port) + "/isBusy")
             except:
                 inactive.append(LoadBalancer.systems[i])
                 continue
-            if(response.json()["busy"]):
+            if (response.json()["busy"]):
                 busy.append(LoadBalancer.systems[i])
             else:
                 not_busy.append(LoadBalancer.systems[i])
@@ -152,10 +168,62 @@ class HelperFunctions:
     def hello_world():
         return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
+    @app.route('/results')
+    def send_recognized_image_response():
+        if(str(results_dict[request.remote_addr])== "None"):
+            return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
+        elif(str(results_dict[request.remote_addr])== "None"):
+            return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
+        else:
+            return json.dumps({'success': True, "data": results_dict[request.remote_addr]}), 200, {'ContentType': 'application/json'}
+
+    def run():
+        while True:
+            # check for files in static
+            files = [] 
+            files = os.listdir("static")
+            for file in files:
+                # check if LoadBalancer.systems[0] is active
+                # HelperFunctions.send_image("static/" + file, LoadBalancer.systems[0])
+                # os.remove  ("static/" + file)
+                if test_this_server(LoadBalancer.systems[0]) == "200":
+                    rec = send_image("static/" + file, LoadBalancer.systems[0])
+                    # print(rec)
+                    # figure out the queueID between -()
+                    queueID = file.split("-")[1].split(".")[0]
+                    results_dict[queueID] = rec
+                    print("RECOGNIZED:" + rec)
+                    os.remove  ("static/" + file)
+                elif test_this_server(LoadBalancer.systems[1]) == "200":
+                    rec = send_image("static/" + file, LoadBalancer.systems[1])
+                    # print(rec)
+                    # figure out the queueID between -()
+                    queueID = file.split("-")[1].split(".")[0]
+                    print("RECOGNIZED:" + rec)
+                    results_dict[queueID] = rec
+                    os.remove  ("static/"+ file)
+                elif test_this_server(LoadBalancer.systems[2]) == "200":
+                    rec = send_image("static/" + file, LoadBalancer.systems[2])
+                    queueID = file.split("-")[1].split(".")[0]
+                    results_dict[queueID] = rec
+                    print("RECOGNIZED:" + rec)
+                    os.remove  ("static/"+ file)
+                else:
+                    print("No servers active")
+
+
 if __name__ == '__main__':
-    try:
-        # app.run(host="0.0.0.0", port=metadata.DEFAULT_PORT, debug=True)
-        print(test_this_server(LoadBalancer.systems[0]))
-        print(send_image("static/dog1.jpeg", LoadBalancer.systems[0]))
-    except:
-        app.run(host="0.0.0.0", port=metadata.DEFAULT_PORT+1, debug=True)
+
+    while(True):
+        try:
+            my_thread = threading.Thread(target=HelperFunctions.run)
+            my_thread.start()
+            app.run(host="0.0.0.0", port=metadata.DEFAULT_PORT+1, debug=True)
+        # print(test_this_server(LoadBalancer.systems[0]))
+        # print(test_this_server(LoadBalancer.systems[1]))
+        # print(test_this_server(LoadBalancer.systems[2]))
+
+        # print(send_image("static/dog1.jpeg", LoadBalancer.systems[0]))
+        except:
+            app.run(host="0.0.0.0", port=metadata.DEFAULT_PORT+1, debug=True)
+        # print("Error")
