058f2c2ea24265dc01f1f837361e3bf7520291e3
Finn Jensen <fjensen@bu.edu>
2022-12-08 19:38:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ class Metadata:
     busy = False
 
     @app.route("/isBusy", methods=['GET'])
-    def isBusy(cls):
+    def isBusy():
         return str(Metadata.busy)
 
 
-@app.route("/imagenet", methods=['POST'])
+@app.route("/", methods=['POST'])
 def imagenet():
     if request.method == "POST":
         Metadata.busy = True
-        image = Image.open(request.form['myImage'])
+        image = Image.open(request.files['myImage'])
         inputs = feature_extractor(images=image, return_tensors="pt")
         outputs = model(**inputs)
         logits = outputs.logits
@@ -33,9 +33,15 @@ def imagenet():
     print("Server Listening")
 
 
+@app.route("/", methods=["GET"])
+def main():
+    Metadata.busy = False
+    return "200: Connection Init"
+
+
 @app.route("/test", methods=['GET'])
 def test():
-    if Metadata.isBusy:
+    if Metadata.busy:
         return "503"
     else:
         return "200"
@@ -43,45 +49,3 @@ def test():
 
 if __name__ == '__main__':
     app.run()
-
-# print_lock = threading.Lock()
-# import socket
-# from _thread import start_new_thread
-# import threading
-# def threaded(c):
-#     while True:
-#         # Get Data from Client
-#         data = c.recv(4096)
-#         if not data:
-#             print_lock.release()
-#             break
-#
-#         # Use Image URL to keep
-#         image = Image.open(requests.get(data, stream=True).raw)
-#         inputs = feature_extractor(images=image, return_tensors="pt")
-#         outputs = model(**inputs)
-#         logits = outputs.logits
-#
-#         # Model predicts one of 21,841 classes
-#         predicted_class_idx = logits.argmax(-1).item()
-#         prediction = model.config.id2label[predicted_class_idx]
-#
-#         # Send Back Data
-#         c.send(prediction)
-#
-#     c.close()
-#
-#
-# def main():
-#     port = 12345
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     server_socket.bind((socket.gethostname, port))
-#     server_socket.listen(5)
-#
-#     while True:
-#         c, addr = server_socket.accept()
-#         print_lock.acquire()
-#
-#         # Start new Thread and return id
-#         start_new_thread(threaded, (c,))
-#     server_socket.close()
